8dc01d359b5574a07723d1f7624ebfb3449b9bcf
Grady Weyenberg <gradysw@hawaii.edu>
2023-10-04 16:24:11 -1000

    inspect libpaths

--- .github/autograder.R
+++ .github/autograder.R
@@ -1,6 +1,5 @@
 .libPaths()
 .Library.site
 Sys.getenv()
-installed.packages()[,c("Package", "Version")]
-library(testthat)
+installed.packages()[,c("Package", "LibPath", "Version")]
 testthat::test_file("tests.R", reporter=c("compactProgress", "fail"))
