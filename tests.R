ae839fb5a4be900ca1e5d4f34baf80cb7c0b6a42
Grady Weyenberg <gradysw@hawaii.edu>
2023-10-04 17:41:14 -1000

    remove fail for no solution file, so first action builds cache successfully on first push

--- tests.R
+++ tests.R
@@ -1,7 +1,7 @@
 solution_file <- fs::dir_ls(glob="*solution.Rmd", ignore.case=TRUE)
 
 test_that("Checking Solution Document",{
-  expect_gt(length(solution_file), 0)
+  #expect_gt(length(solution_file), 0)
   for(fn in solution_file)
     expect_no_error(knitr::knit(text=readLines(fn)))
 })
