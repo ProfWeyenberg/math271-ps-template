b37c1c34d022eabc4d47c7c1b39af016f4fd0a94
Grady Weyenberg <gradysw@hawaii.edu>
2023-10-04 15:41:37 -1000

    reenable testthat

--- .github/autograder.R
+++ .github/autograder.R
@@ -1,1 +1,2 @@
-#testthat::test_file("tests.R", reporter=c("compactProgress", "fail"))
+installed.packages()[,c("Package", "Version")]
+testthat::test_file("tests.R", reporter=c("compactProgress", "fail"))
