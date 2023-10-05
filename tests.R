7b0d1328bfd32d7389963ac3e3fad1b4fa6fadbb
Grady Weyenberg <gradysw@hawaii.edu>
2023-10-05 13:09:38 -1000

    autograder expect solution file

--- tests.R
+++ tests.R
@@ -1,8 +1,8 @@
 solution_file <- fs::dir_ls(glob="*solution.Rmd", ignore.case=TRUE)
 
 test_that("Checking Solution Document",{
-  #expect_gt(length(solution_file), 0)
   if(length(solution_file) == 0L) warning("No solution files found.")
+  expect_gt(length(solution_file), 0)
   for(fn in solution_file)
     expect_no_error(knitr::knit(text=readLines(fn)))
 })
